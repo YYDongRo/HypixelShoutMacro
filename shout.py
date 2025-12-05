d307f3b05eeb02b7bdea5ebcb1f8dcf1753563a3
Roy Dong <157320193+YYDongRo@users.noreply.github.com>
2025-12-04 20:21:26 -0800

    Update shout.py

--- shout.py
+++ shout.py
@@ -2,16 +2,18 @@ from pynput import keyboard
 import pyautogui
 import time
 
-SHOUT_MESSAGE = "/shout Like squeezing a lemon LOLL!"
+SHOUT_MESSAGE = "/shout Like squeezing a lemon"
 
 # Replace this with the key you want to press
-TRIGGER_KEY = keyboard.Key.f6
+TRIGGER_KEY = keyboard.Key.f12
 
 
 def send_shout():
     # Type the message
+    pyautogui.typewrite("t")
+    time.sleep(0.5)
     pyautogui.typewrite(SHOUT_MESSAGE)
-    time.sleep(0.05)
+    time.sleep(0.5)
     pyautogui.press("enter")
 
 
@@ -21,5 +23,6 @@ def on_press(key):
 
 
 with keyboard.Listener(on_press=on_press) as listener:
-    print("Shout macro running... Press F6 to send the message.")
+    print("Shout macro running... Press f12 to send the message.")
     listener.join()
+
